394b51df0c21736e50fee623d62477a852c63220
Pondo 2 <pondo2@iMac-de-Pondo.local>
2023-02-22 17:30:37 +0100

    g

---
+++ resources/views/customer_mail.blade.php
@@ -0,0 +1,15 @@
+<br/>
+Bonjour!
+<br/><br/>
+
+Nous avons le plaisir de vous informer que {{$customer_details['number']}} matricules vienent d'être générés avec succès.
+<br/><br/>
+
+<strong>Informations supplémentaires</strong><br/>
+Code CSE utilisé : {{$customer_details['cse']}}<br/>
+
+
+<br/> <br/> 
+
+Merci d'avoir d'utiliser Creuch.
+<br/>
