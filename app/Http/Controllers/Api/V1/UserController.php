7dc195b7ecac18670bb0a9469037db2a2da5289a
Pondo 2 <pondo2@iMac-de-Pondo.local>
2023-02-25 19:17:53 +0100

    g

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Api\V1\BaseController as BaseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Validator;
- 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Str;
